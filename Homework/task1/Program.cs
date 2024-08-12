9d0de230a94c131dfb1af926ef46f68f27f63fb9
Soulbring <lobakin_vova@vk.com>
2024-08-12 10:54:30 +0400

    Add Homework:Task1

---
+++ Homework/task1/Program.cs
@@ -0,0 +1,45 @@
+﻿//Напишите программу, которая бесконечно запрашивает целые числа с консоли.
+//Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого чётная.
+
+
+int DoNumberSumm (int Number)
+{
+    int sum = 0;
+    int digit = 0;
+    while(Number > 0)
+    {
+        digit = Number % 10;
+        sum = sum + digit;
+        Number = Number / 10;
+    }
+    return sum;
+}
+
+while (true)
+{
+    Console.WriteLine("Введите целое число:");
+    string NumberString = Console.ReadLine();
+    if (NumberString == "q")
+    {
+        Console.WriteLine("Вы ввели q. Программа завершается");
+        break;
+    }
+
+    int Number = Convert.ToInt32(NumberString);
+    int sum = DoNumberSumm(Number);
+    if (NumberString == "q" || sum % 2 == 0)
+    {
+        Console.WriteLine("Cумма цифр числа четная. Программа завершается");
+        break; 
+    }
+    else
+    {
+        Console.WriteLine($"Сумма чисел равна {sum}");
+    }
+}
+
+
+
+
+
+
---
+++ Homework/task2/Program.cs
@@ -0,0 +1,11 @@
+﻿    int sum = 0;
+    int digit = 0;
+    int Number = 15252;
+
+    while(Number > 0)
+    {
+        digit = Number % 10;
+        sum = sum + digit;
+        Number = Number / 10;
+    }
+    Console.WriteLine(sum);
